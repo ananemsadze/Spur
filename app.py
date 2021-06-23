cffcd7040cad07a9db914862b29cbcb17cb2f2b0
ananemsadze <anemsadze@affix.net>
2021-06-23 14:22:19 +0400

    APIS

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = "getYourLifeTogether"
@@ -15,22 +17,72 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(12), nullable=False)
 
+class Quotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    quote = db.Column(db.String(1200), unique=False, nullable=False)
+
+db.create_all()
 
 all_user_dict = {}
 all_users = Users.query.all()
 for un in all_users:
-    all_user_dict[un.email] = un.username
+    all_user_dict[un.email] = {un.username: un.password}
+
+all_username = []
+for email in all_user_dict:
+    username = list(all_user_dict[email].keys())[0]
+    all_username.append(username)
+
+
+# </ ------------------------------------------------------------------------------------------------------------------
+
+url_bored = "http://www.boredapi.com/api/activity/"
+url_affirmation = "https://www.affirmations.dev/"
+url_advice = "https://api.adviceslip.com/advice"
+
+BORED = "activity"
+AFFIRMATION = "affirmation"
+ADVICE_ONE = "slip"
+ADVICE_TWO = "advice"
+
+def api_service(url, key, second_key=None):
+    r = requests.get(url)
+    json_api = r.text
+    dict_api = json.loads(json_api)
+    if second_key is None:
+        return dict_api[key]
+    else:
+        return dict_api[key][second_key]
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/addQuote/<string:quote>/<string:page>',methods=['POST'])
+def addQuote(quote,page):
+    quote = Quotes(username=session["username"],  quote=quote)
+    db.session.add(quote)
+    db.session.commit()
+    return redirect('/'+str(page))
 
-@app.route('/Motivation')
+@app.route('/removeQuote/<string:quote>',methods=['POST'])
+def removeQuote(quote):
+    all_quotes = Quotes.query.all()
+    for un in all_quotes:
+        if un.username == session["username"] and un.quote == quote:
+            db.session.delete(un)
+            db.session.commit()
+
+    return redirect('/MyProfile')
+
+@app.route('/Motivation',methods=['GET'])
 def motivation():
-    return render_template("motivation.html")
+    quote = api_service(url_affirmation, AFFIRMATION)
+    return render_template("motivation.html", quote=quote)
 
 
 @app.route('/Login', methods=['POST', 'GET'])
@@ -38,10 +90,17 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         if email in all_user_dict.keys():
-            session.permanent = True
-            session['email'] = email
-            session['username'] = all_user_dict[email]
-            return redirect(url_for('home'))
+            password = request.form["password"]
+            if password == all_user_dict[un.email][list(all_user_dict[un.email])[0]]:
+                session.permanent = True
+                session['email'] = email
+
+                session['username'] = list(all_user_dict[email].keys())[0]
+                return redirect(url_for('home'))
+            else:
+                return redirect("/Login")
+        else:
+            return redirect("/Login")
 
     return render_template("login.html")
 
@@ -53,25 +112,31 @@ def register():
         email = request.form['email']
         password = request.form['password']
         repeat_password = request.form['repeatPassword']
-        if(username not in all_user_dict.values() and email not in all_user_dict.keys()) and password == repeat_password:
+        if(username not in all_username and email not in all_user_dict.keys()) and password == repeat_password:
             session.permanent = True
             session['email'] = email
             session['username'] = username
-            user = Users(username=username, email=email)
+            user = Users(username=username, email=email,password=password)
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('home'))
-    return render_template("signup.html")
+        else:
+            return redirect("/SignUp")
+
+    elif request.method == 'GET':
+        return render_template("signup.html")
 
 
-@app.route('/Advices')
+@app.route('/Advices',methods=['GET'])
 def advices():
-    return render_template("advices.html")
+    advice = api_service(url_advice, ADVICE_ONE, ADVICE_TWO)
+    return render_template("advices.html",advice=advice)
 
 
-@app.route('/Bored')
+@app.route('/Bored',methods=['GET'])
 def bored():
-    return render_template("bored.html")
+    quote = api_service(url_bored, BORED)
+    return render_template("bored.html", quote=quote)
 
 
 @app.route('/AboutUs')
@@ -81,7 +146,12 @@ def about():
 
 @app.route('/MyProfile')
 def profile():
-    return render_template("myprofile.html")
+    all_quotes = Quotes.query.all()
+    set_of_quotes = set()
+    for un in all_quotes:
+        if un.username == session["username"]:
+            set_of_quotes.add(un.quote)
+    return render_template("myprofile.html", set_of_quotes=set_of_quotes)
 
 
 @app.route('/signout')
